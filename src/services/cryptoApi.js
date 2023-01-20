0ca5c079747443c41bbf24485416176769a5a17c
Shashank-09 <shashanktulsikar979@gmail.com>
2023-01-21 01:12:06 +0530

    Initial Commit

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,28 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+
+    'X-RapidAPI-Key': '14d7b52f16msh3561c0130652a4dp1aba44jsn78c9017b99fe',
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
+};
+const baseUrl = 'https://coinranking1.p.rapidapi.com'
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+      getCryptos: builder.query({
+        query: (count) => createRequest(`/coins?limit=${count}`),
+      }),
+      getCryptoDetails: builder.query({
+        query: (coinId) => createRequest(`/coin/${coinId}`),
+      })
+      
+    })
+});
+export const {
+    useGetCryptosQuery,
+    useGetCryptoDetailsQuery
+  } = cryptoApi;
